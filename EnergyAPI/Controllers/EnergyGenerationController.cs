9d3b2f17bb9361815aea9b00dc644fdf4d7e980b
JoelQ <joelquey@hotmail.com>
2021-11-04 23:18:22 +0800

    Added cache for get endpoints

--- EnergyAPI/Controllers/EnergyGenerationController.cs
+++ EnergyAPI/Controllers/EnergyGenerationController.cs
@@ -28,24 +28,31 @@ namespace EnergyAPI.Controllers {
         public async Task<IEnumerable<EnergyGeneration>> GetEnergyGenerations([FromQuery] int? page, [FromQuery] EnergyGenerationFilter filters) {
 
             // Need to log query
-            var energyGenerations = await dbContext.EnergyGeneration.AsNoTracking().FilterEnergyGeneration(filters).FilterPage(page).ToListAsync();
+            var energyGenerations = await cache.GetCacheAsync<List<EnergyGeneration>>($"{page}{JsonSerializer.Serialize(filters)}");
+
+            if(energyGenerations != null) {
+                return energyGenerations;
+            }
+
+            energyGenerations = await dbContext.EnergyGeneration.AsNoTracking().FilterEnergyGeneration(filters).FilterPage(page).ToListAsync();
+            await cache.SetCacheAsync($"{page}{JsonSerializer.Serialize(filters)}", energyGenerations);
 
             return energyGenerations;
         }
 
         [HttpGet("{id}")]
-        public async Task<string> GetEnergyGeneration(int id) {
+        public async Task<EnergyGeneration> GetEnergyGeneration(int id) {
 
            var energyGeneration = await cache.GetCacheAsync<EnergyGeneration>($"{id}");
             // Need to log query
             if(energyGeneration != null) {
-                return JsonSerializer.Serialize(new { source = energyGeneration, message = "cache" });
+                return energyGeneration;
             }
 
             energyGeneration = dbContext.EnergyGeneration.Where(eg => eg.Id == id).Single();
             await cache.SetCacheAsync($"{id}", energyGeneration);
 
-            return JsonSerializer.Serialize(new { source = energyGeneration, message = "non-cache" });
+            return energyGeneration;
         }
 
         [HttpPost("add")]
